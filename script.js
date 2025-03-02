ae5ca94a7fc7db8c5ac33cc0e260de0ade590cdf
gchristiansun <167200722+gchristiansun@users.noreply.github.com>
2025-03-02 08:00:47 +0700

    rev

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ menuButton.addEventListener("click", function() {
     navbar.classList.toggle("active");
 })
 
-const threeDot = document.getElementById("asideButton")
-const aside = document.querySelector("aside .social-media ul");
+const threeDot = document.getElementById("threedotButton")
+const aside = document.querySelector(".social-media ul");
 
 threeDot.addEventListener("click", function() {
     aside.classList.toggle("active")
